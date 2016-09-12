3290b5b1cbfffedd70d556483bb435491773bca2
Victor Qi <qijianqiong@gmail.com>
2016-09-12 13:15:28 +0800

    Initial Commit

---
+++ Core iOS Learning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core iOS Learning
+//
+//  Created by v.q on 16/9/12.
+//  Copyright © 2016年 Victor Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Core iOS Learning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core iOS Learning
+//
+//  Created by v.q on 16/9/12.
+//  Copyright © 2016年 Victor Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
